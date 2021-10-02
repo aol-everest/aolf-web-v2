d2060e978ea38dfb4a8e7db9ca5fcb9161912000
xenotime <sandeepkhoj@gmail.com>
2021-10-02 16:25:12 +0530

    bug fix

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,4 @@
 export { useIntersectionObserver } from "./useIntersectionObserver";
 export { useQueryString } from "./useQueryString";
 export { useScroll } from "./useScroll";
-export { useMediaQuery } from "./useMediaQuery";
 export { useGeolocation } from "./useGeolocation";
