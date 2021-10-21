7d29634e58a928c99083f28c73381ccfb0a918d4
xenotime <sandeepkhoj@gmail.com>
2021-10-21 12:40:40 +0530

    fixed token page

--- pages/token/index.jsx
+++ pages/token/index.jsx
@@ -1,28 +1,24 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { Auth } from "aws-amplify";
-import { withSSRContext } from "aws-amplify";
-import { useGlobalModalContext } from "@contexts";
-import { MODAL_TYPES } from "@constants";
-import { api } from "@utils";
+import { Loader } from "@components";
 
-export const getServerSideProps = async (context) => {
-  const { query, req, res } = context;
-  const { next } = query;
-  const { Auth } = await withSSRContext(context);
-  const user = await Auth.currentAuthenticatedUser();
-  const token = user.signInUserSession.idToken.jwtToken;
-  await api.get({
-    path: "profile",
-    token,
-  });
-  res.writeHead(302, {
-    Location: next ? next : `/`,
-  });
-  res.end();
+// export const getServerSideProps = async (context) => {
+// const { query, req, res } = context;
+// const { next } = query;
+// const { Auth } = await withSSRContext(context);
+// const user = await Auth.currentAuthenticatedUser();
+// const token = user.signInUserSession.idToken.jwtToken;
+// await api.get({
+//   path: "profile",
+//   token,
+// });
+//   res.writeHead(302, {
+//     Location: `/`,
+//   });
+//   res.end();
 
-  return { props: {} };
-};
+//   return { props: {} };
+// };
 
 function Token() {
   const router = useRouter();
@@ -31,7 +27,11 @@ function Token() {
     router.replace("/");
   });
 
-  return null;
+  return (
+    <main className="aol_mainbody login-screen">
+      <Loader />
+    </main>
+  );
 }
 Token.hideHeader = true;
 
