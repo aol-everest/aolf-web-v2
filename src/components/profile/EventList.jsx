48925cf622fa87ab5f1e369f7c4708a2dd67c1c3
xenotime <sandeepkhoj@gmail.com>
2021-12-21 20:11:04 +0530

    ui fix

--- src/components/profile/EventList.jsx
+++ src/components/profile/EventList.jsx
@@ -228,7 +228,7 @@ const renderEvent = (workshop) => {
     }
     return (
       <div className="col-6 col-lg-3 col-md-4" key={sfid}>
-        <div className="profile-body__card tw-bg-transparent">
+        <div className="profile-body__card !tw-bg-transparent">
           <img src={imageSrc} alt="bg" className="profile-body__card-img" />
           {moment
             .utc(eventStartDate)
