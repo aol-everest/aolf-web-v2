636e197d161e0db614ca5f90503c244e1fcf6f02
xenotime <sandeepkhoj@gmail.com>
2022-04-14 19:53:02 +0530

    added Cache clean button

--- pages/__devonly.jsx
+++ pages/__devonly.jsx
@@ -1,5 +1,6 @@
 import { withSSRContext } from "aws-amplify";
 import { api } from "@utils";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps({ req }) {
   const { Auth } = withSSRContext({ req });
@@ -28,15 +29,42 @@ export async function getServerSideProps({ req }) {
 }
 
 function DevOnly({ profile = {} }) {
+  const router = useRouter();
+  const { log } = router.query;
+  const clearCaches = () => {
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          return caches.delete(key);
+        }),
+      );
+    });
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+    alert("Complete Cache Cleared");
+    window.location.reload(true);
+  };
   return (
-    <div className="tw-bg-gray-200 tw-text-white tw-text-sm">
-      <div className="tw-bg-gray-500 tw-px-4 tw-py-3 tw-text-yellow-300">
-        <strong>Debug</strong>
+    <>
+      <div className="tw-p-5 tw-text-center">
+        <button className="btn btn-warning" onClick={clearCaches}>
+          Reload App
+        </button>
       </div>
-      <pre className="tw-block tw-px-4 tw-py-3 tw-m-0 tw-overflow-scroll">
-        {JSON.stringify(profile, null, 2)}
-      </pre>
-    </div>
+      {log && (
+        <div className="tw-bg-gray-200 tw-text-white tw-text-sm">
+          <div className="tw-bg-gray-500 tw-px-4 tw-py-3 tw-text-yellow-300">
+            <strong>Debug</strong>
+          </div>
+          <pre className="tw-block tw-px-4 tw-py-3 tw-m-0 tw-overflow-scroll">
+            {JSON.stringify(profile, null, 2)}
+          </pre>
+        </div>
+      )}
+    </>
   );
 }
 
