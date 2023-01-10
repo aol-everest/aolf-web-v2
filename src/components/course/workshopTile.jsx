1faf679dc967a4f7c2d00687c97d2a770e9caf74
Ritviz Sharma <ritvizsharms@Ritvizs-MacBook-Pro.local>
2023-01-10 21:42:52 +0530

    [Fix/course_redirect] Course redirect fix added

--- src/components/course/workshopTile.jsx
+++ src/components/course/workshopTile.jsx
@@ -52,7 +52,10 @@ export const WorkshopTile = ({ data, authenticated }) => {
     COURSE_TYPES.VOLUNTEER_TRAINING_PROGRAM.value.indexOf(productTypeId) >= 0 ||
     COURSE_TYPES.HEALING_BREATH.value.indexOf(data.productTypeId) >= 0 ||
     COURSE_TYPES.SKY_SILENT_RETREAT.value.indexOf(data.productTypeId) >= 0 ||
-    COURSE_TYPES.BLESSINGS_COURSE.value.indexOf(data.productTypeId) >= 0;
+    COURSE_TYPES.BLESSINGS_COURSE.value.indexOf(data.productTypeId) >= 0 ||
+    COURSE_TYPES.SKY_CAMPUS_HAPPINESS_RETREAT.value.indexOf(productTypeId) >=
+      0 ||
+    COURSE_TYPES.SANYAM_COURSE.value.indexOf(productTypeId) >= 0;
 
   const detailAction = (workshopId, productTypeId) => () => {
     if (isKnownWorkshop) {
