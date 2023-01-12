aca76febe40367731649296450f0327580099694
xenotime <sandeepkhoj@gmail.com>
2023-01-12 19:58:03 +0530

    - check env NEXT_PUBLIC_CLEVERTAP_ACCOUNT_ID

--- src/components/clevertapAnalytics/index.jsx
+++ src/components/clevertapAnalytics/index.jsx
@@ -1,20 +1,23 @@
 import Script from "next/script";
 
 export default function ClevertapAnalytics() {
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        id="clevertap-script"
-        src="https://d2r1yp2w7bby2u.cloudfront.net/js/clevertap.min.js"
-      />
-      <Script id="clevertap-analytics" strategy="afterInteractive">
-        {`var clevertap = {event:[], profile:[], account:[], onUserLogin:[], notifications:[], privacy:[]};
+  if (process.env.NEXT_PUBLIC_CLEVERTAP_ACCOUNT_ID) {
+    return (
+      <>
+        <Script
+          strategy="afterInteractive"
+          id="clevertap-script"
+          src="https://d2r1yp2w7bby2u.cloudfront.net/js/clevertap.min.js"
+        />
+        <Script id="clevertap-analytics" strategy="afterInteractive">
+          {`var clevertap = {event:[], profile:[], account:[], onUserLogin:[], notifications:[], privacy:[]};
         clevertap.account.push({"id": "${process.env.NEXT_PUBLIC_CLEVERTAP_ACCOUNT_ID}"});
         clevertap.privacy.push({optOut: false}); 
         clevertap.privacy.push({useIP: true});
         clevertap.spa = true`}
-      </Script>
-    </>
-  );
+        </Script>
+      </>
+    );
+  }
+  return null;
 }
