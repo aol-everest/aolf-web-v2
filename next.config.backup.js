b7c398c83a0ccde6b6a56bf28fc597c931b672b3
Praveen <praveengupta@cloudalgo.com>
2023-07-18 16:51:57 +0530

    Upgrade sentry

--- next.config.backup.js
+++ next.config.backup.js
@@ -1,5 +1,5 @@
 const withPlugins = require("next-compose-plugins");
-// const { withSentryConfig } = require("@sentry/nextjs");
+const { withSentryConfig } = require("@sentry/nextjs");
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
@@ -48,49 +48,52 @@ const securityHeaders = [
   },
 ];
 
-module.exports = withPlugins([withBundleAnalyzer], {
-  swcMinify: true,
-  // basePath: "/us-en",
-  // assetPrefix: "/us-en/",
-  productionBrowserSourceMaps: true,
-  images: {
-    domains: ["images.ctfassets.net"],
-  },
+module.exports = withPlugins(
+  [withBundleAnalyzer, withSentryConfig, SentryWebpackPluginOptions],
+  {
+    swcMinify: true,
+    // basePath: "/us-en",
+    // assetPrefix: "/us-en/",
+    productionBrowserSourceMaps: true,
+    images: {
+      domains: ["images.ctfassets.net"],
+    },
 
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/us-en/course",
-        permanent: true,
-      },
-      {
-        source: "/us-en",
-        destination: "/us-en/course",
-        permanent: true,
-      },
-    ];
+    async redirects() {
+      return [
+        {
+          source: "/",
+          destination: "/us-en/course",
+          permanent: true,
+        },
+        {
+          source: "/us-en",
+          destination: "/us-en/course",
+          permanent: true,
+        },
+      ];
+    },
+    async headers() {
+      return [
+        {
+          // Apply these headers to all routes in your application.
+          source: "/(.*)",
+          headers: securityHeaders,
+        },
+        {
+          source: "/:all*(svg|jpg|png)",
+          locale: false,
+          headers: [
+            {
+              key: "cache-control",
+              value: "public, max-age=800 must-revalidate",
+            },
+          ],
+        },
+      ];
+    },
   },
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-      {
-        source: "/:all*(svg|jpg|png)",
-        locale: false,
-        headers: [
-          {
-            key: "cache-control",
-            value: "public, max-age=800 must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
-});
+);
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
