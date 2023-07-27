03c856ac431d1db8f09b764d5a063bed91bc4040
xenotime <sandeepkhoj@gmail.com>
2023-07-27 20:57:02 +0530

    fix(fixed singlequote lint): fixed singlequote lint

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   semi: true,
-  trailingComma: "all",
-  singleQuote: false,
+  trailingComma: 'all',
+  singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
-  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
 };
