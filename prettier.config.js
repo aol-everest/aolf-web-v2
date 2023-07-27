11beff6069a920bf946c678a6833de0fc7ffae02
xenotime <sandeepkhoj@gmail.com>
2023-07-27 21:01:06 +0530

    fix: lint fix

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
+  trailingComma: "all",
+  singleQuote: false,
   printWidth: 80,
   tabWidth: 2,
-  plugins: ['prettier-plugin-organize-imports', 'prettier-plugin-tailwindcss'],
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
 };
