6d84a503edaadd784813f0f4bc6ea8f98755e974
xenotime <sandeepkhoj@gmail.com>
2023-07-31 16:31:26 +0530

    fix: updated helmet

--- server.js
+++ server.js
@@ -17,18 +17,12 @@ app.prepare().then(() => {
   server.disable("x-powered-by");
   // server.use(helmet.contentSecurityPolicy());
   // server.use(helmet.crossOriginEmbedderPolicy());
-  server.use(helmet.crossOriginOpenerPolicy());
-  server.use(helmet.crossOriginResourcePolicy());
-  server.use(helmet.dnsPrefetchControl());
-  server.use(helmet.frameguard());
-  server.use(helmet.hidePoweredBy());
-  server.use(helmet.hsts());
-  server.use(helmet.ieNoOpen());
-  server.use(helmet.noSniff());
-  server.use(helmet.originAgentCluster());
-  server.use(helmet.permittedCrossDomainPolicies());
-  server.use(helmet.referrerPolicy());
-  server.use(helmet.xssFilter());
+  server.use(
+    helmet({
+      contentSecurityPolicy: false,
+      crossOriginEmbedderPolicy: false,
+    }),
+  );
   // server.use(nocache());
 
   server.all("*", (req, res) => {
