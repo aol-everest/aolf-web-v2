6b8272d94b2275e37c555a17298bbc687b946380
Praveen <praveengupta@cloudalgo.com>
2023-08-02 22:10:23 +0530

    fix: fix - 500 error on checkout

--- pages/us-en/course/checkout/[id].jsx
+++ pages/us-en/course/checkout/[id].jsx
@@ -268,7 +268,7 @@ const Checkout = () => {
     0;
 
   const isStripeIntentPayment =
-    workshop.otherPaymentOptions.indexOf("Stripe Intent Payment") >= 0;
+    workshop?.otherPaymentOptions?.indexOf("Stripe Intent Payment") >= 0;
 
   const renderPaymentForm = () => {
     if (isHealingBreathProgram || isInstitutionalProgram) {
