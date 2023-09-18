0ad7d2ddb95332bb1d63590185187fd7fa4ba344
Praveen Gupta <129676978+praveencloudalgo@users.noreply.github.com>
2023-09-18 16:28:44 +0530

    fix: silent Retreat Landing page added (#248)

---
+++ pages/us-en/course/landing/silent_retreat.jsx
@@ -0,0 +1,11 @@
+import { SKYSilentRetreat } from "@components/courseDetails";
+import { NextSeo } from "next-seo";
+
+export default function SilentRetreat() {
+  return (
+    <>
+      <NextSeo title="Silent retreat" />
+      <SKYSilentRetreat data={{ title: "Silent retreat" }} />
+    </>
+  );
+}
