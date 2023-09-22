46717692bf1a51e474b4896feae72f408bf03ed4
Praveen <praveengupta@cloudalgo.com>
2023-09-22 10:12:28 +0530

    fix: api integration

--- pages/us-en/ticketed-event/[id].jsx
+++ pages/us-en/ticketed-event/[id].jsx
@@ -1,19 +1,79 @@
+import { api, tConvert } from "@utils";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import { ABBRS } from "@constants";
+
+dayjs.extend(utc);
 
 export default function TicketedEvent() {
   const router = useRouter();
+  const [selectedTickets, setSelectedTickets] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
   const { id: workshopId } = router.query;
+
+  const { data: workshop } = useQuery(
+    "getTicketedEvent",
+    async () => {
+      const response = await api.get({
+        path: "getTicketedEvent",
+        param: {
+          id: workshopId,
+        },
+      });
+      return response.data;
+    },
+    {
+      refetchOnWindowFocus: false,
+      enabled: !!workshopId,
+    },
+  );
+
+  const {
+    eventStartTime,
+    eventEndTime,
+    title,
+    eventStartDate,
+    eventTimeZone,
+    pricingTiers,
+  } = workshop || {};
+
+  useEffect(() => {
+    let totalPrice = 0;
+    selectedTickets.map((item) => {
+      totalPrice = totalPrice + item.price;
+    });
+    setTotalPrice(totalPrice);
+  }, [selectedTickets]);
+
+  const handleTicketSelect = (e, type, item) => {
+    if (type === "add") {
+      setSelectedTickets((prevState) => {
+        return [...prevState, item];
+      });
+    } else {
+      const filteredItem = selectedTickets.filter(
+        (newItem) => newItem.ruleId !== item.ruleId,
+      );
+      setSelectedTickets([...filteredItem]);
+    }
+  };
+
+  console.log("selectedTickets", selectedTickets);
 
   return (
     <div class="tickets-modal">
       <div class="tickets-modal__container products">
         <div class="tickets-modal__left-column">
           <div class="tickets-modal__section-products">
-            <h2 class="tickets-modal__title">
-              The Journey Within - Wisdom and Meditation with Gurudev
-            </h2>
-            <p class="tickets-modal__date">Wednesday, July 26 • 6 - 7 pm CDt</p>
+            <h2 class="tickets-modal__title">{title}</h2>
+            <p class="tickets-modal__date">
+              {`${dayjs.utc(eventStartDate).format("dddd, MMM DD ")}`}•{" "}
+              {tConvert(eventStartTime, true)} - {tConvert(eventEndTime, true)}{" "}
+              {" " + ABBRS[eventTimeZone]}
+            </p>
 
             <div class="tickets-modal__promo">
               <label class="tickets-modal__promo-label" for="">
@@ -35,169 +95,60 @@ export default function TicketedEvent() {
             </div>
 
             <div class="tickets-modal__list">
-              <div class="tickets-modal__card">
-                <div class="tickets-modal__card-head">
-                  <h3 class="tickets-modal__card-name">Silver</h3>
+              {pricingTiers?.map((item, index) => {
+                const selectedIds = selectedTickets.map(
+                  (ticket) => ticket.ruleId,
+                );
+                return (
+                  <div class="tickets-modal__card" key={item.ruleId}>
+                    <div class="tickets-modal__card-head">
+                      <h3 class="tickets-modal__card-name">
+                        {item.pricingTierName}
+                      </h3>
 
-                  <div class="tickets-modal__counter">
-                    <button
-                      class="tickets-modal__counter-button"
-                      data-counter="decrease"
-                      data-product="product-1"
-                    >
-                      -
-                    </button>
-                    <input
-                      class="tickets-modal__counter-input"
-                      type="number"
-                      name="product-1"
-                      id="product-1"
-                      value="0"
-                      min="0"
-                      max="10"
-                    />
-                    <button
-                      class="tickets-modal__counter-button"
-                      data-counter="increase"
-                      data-product="product-1"
-                    >
-                      +
-                    </button>
+                      <div class="tickets-modal__counter">
+                        <button
+                          class="tickets-modal__counter-button"
+                          data-counter="decrease"
+                          data-product={`product-${index + 1}`}
+                          onClick={(e) => handleTicketSelect(e, "remove", item)}
+                          disabled={!selectedIds.includes(item.ruleId)}
+                        >
+                          -
+                        </button>
+                        <input
+                          class="tickets-modal__counter-input"
+                          type="number"
+                          name={item.ruleId}
+                          id={item.ruleId}
+                          value={selectedIds.includes(item.ruleId) ? "1" : "0"}
+                          min="0"
+                          max="1"
+                          onChange={handleTicketSelect}
+                        />
+                        <button
+                          class="tickets-modal__counter-button"
+                          data-counter="increase"
+                          data-product={`product-${index + 1}`}
+                          disabled={selectedIds.includes(item.ruleId)}
+                          onClick={(e) => handleTicketSelect(e, "add", item)}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+
+                    <h4 class="tickets-modal__card-amount">
+                      ${item.price.toFixed(2)}
+                      {/* <span>+ $3.31 Fee</span> */}
+                    </h4>
+
+                    {/* <p class="tickets-modal__card-sale">
+                      Sales end on Jul 26, 2023
+                    </p> */}
                   </div>
-                </div>
-
-                <h4 class="tickets-modal__card-amount">
-                  $22.00
-                  <span>+ $3.31 Fee</span>
-                </h4>
-
-                <p class="tickets-modal__card-sale">
-                  Sales end on Jul 26, 2023
-                </p>
-              </div>
-
-              <div class="tickets-modal__card">
-                <div class="tickets-modal__card-head">
-                  <h3 class="tickets-modal__card-name">Silver</h3>
-
-                  <div class="tickets-modal__counter">
-                    <button
-                      class="tickets-modal__counter-button"
-                      data-counter="decrease"
-                      data-product="product-2"
-                    >
-                      -
-                    </button>
-                    <input
-                      class="tickets-modal__counter-input"
-                      type="number"
-                      name="product-2"
-                      id="product-2"
-                      value="0"
-                      min="0"
-                      max="10"
-                    />
-                    <button
-                      class="tickets-modal__counter-button"
-                      data-counter="increase"
-                      data-product="product-2"
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-
-                <h4 class="tickets-modal__card-amount">
-                  $22.00
-                  <span>+ $3.31 Fee</span>
-                </h4>
-
-                <p class="tickets-modal__card-sale">
-                  Sales end on Jul 26, 2023
-                </p>
-              </div>
-
-              <div class="tickets-modal__card">
-                <div class="tickets-modal__card-head">
-                  <h3 class="tickets-modal__card-name">Silver</h3>
-
-                  <div class="tickets-modal__counter">
-                    <button
-                      class="tickets-modal__counter-button"
-                      data-counter="decrease"
-                      data-product="product-1"
-                    >
-                      -
-                    </button>
-                    <input
-                      class="tickets-modal__counter-input"
-                      type="number"
-                      name="product-1"
-                      id="product-1"
-                      value="0"
-                      min="0"
-                      max="10"
-                    />
-                    <button
-                      class="tickets-modal__counter-button"
-                      data-counter="increase"
-                      data-product="product-1"
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-
-                <h4 class="tickets-modal__card-amount">
-                  $22.00
-                  <span>+ $3.31 Fee</span>
-                </h4>
-
-                <p class="tickets-modal__card-sale">
-                  Sales end on Jul 26, 2023
-                </p>
-              </div>
-
-              <div class="tickets-modal__card">
-                <div class="tickets-modal__card-head">
-                  <h3 class="tickets-modal__card-name">Silver</h3>
-
-                  <div class="tickets-modal__counter">
-                    <button
-                      class="tickets-modal__counter-button"
-                      data-counter="decrease"
-                      data-product="product-2"
-                    >
-                      -
-                    </button>
-                    <input
-                      class="tickets-modal__counter-input"
-                      type="number"
-                      name="product-2"
-                      id="product-2"
-                      value="0"
-                      min="0"
-                      max="10"
-                    />
-                    <button
-                      class="tickets-modal__counter-button"
-                      data-counter="increase"
-                      data-product="product-2"
-                    >
-                      +
-                    </button>
-                  </div>
-                </div>
-
-                <h4 class="tickets-modal__card-amount">
-                  $22.00
-                  <span>+ $3.31 Fee</span>
-                </h4>
-
-                <p class="tickets-modal__card-sale">
-                  Sales end on Jul 26, 2023
-                </p>
-              </div>
+                );
+              })}
             </div>
 
             <div class="tickets-modal__language" />
@@ -211,6 +162,7 @@ export default function TicketedEvent() {
         </div>
 
         <div class="tickets-modal__right-column">
+          {/* <img class="tickets-modal__photo" src={workshop?.coverImage} alt="" /> */}
           <img class="tickets-modal__photo" src="/img/Gurudev_1.png" alt="" />
 
           <div class="tickets-modal__cart-empty">
@@ -219,17 +171,21 @@ export default function TicketedEvent() {
 
           <div class="tickets-modal__cart">
             <h2 class="tickets-modal__cart-summary">Order Summary</h2>
-
-            <p class="tickets-modal__cart-product">
-              x1 SILVER <span>$22</span>
-            </p>
+            {selectedTickets.map((item) => {
+              return (
+                <p class="tickets-modal__cart-product" key={item.ruleId}>
+                  x1 {item.pricingTierName}{" "}
+                  <span>${item.price.toFixed(2)}</span>
+                </p>
+              );
+            })}
 
             <p class="tickets-modal__cart-subtotal">
-              Subtotal <span>$22</span>
+              Subtotal <span>${totalPrice.toFixed(2)}</span>
             </p>
 
             <p class="tickets-modal__cart-total">
-              Total <span>$22</span>
+              Total <span>${totalPrice.toFixed(2)}</span>
             </p>
           </div>
         </div>
