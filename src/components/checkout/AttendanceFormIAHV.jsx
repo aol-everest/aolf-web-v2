00a3445c35c63110df478851052de9f977db22a2
Praveen Gupta <129676978+praveencloudalgo@users.noreply.github.com>
2023-11-02 20:07:35 +0530

    fix: add University affiliate dropwdown (#292)

---
+++ src/components/checkout/AttendanceFormIAHV.jsx
@@ -0,0 +1,39 @@
+import classNames from 'classnames';
+import { Fragment } from 'react';
+import Style from './AttendanceForm.module.scss';
+import { StyledInput } from './StyledInput';
+import { Dropdown } from './Dropdown';
+
+export const AttendanceFormIAHV = ({ formikProps, corporates }) => {
+  let corporateOptions = corporates?.map((corporate) => {
+    return { value: corporate.sfid, label: corporate.corporateName };
+  });
+
+  if (corporateOptions) {
+    corporateOptions = [
+      ...corporateOptions,
+      { value: 'other', label: 'Other' },
+    ];
+  }
+
+  return (
+    <Fragment>
+      <Dropdown
+        placeholder="Healthcare Organization"
+        formikProps={formikProps}
+        formikKey="contactHealthcareOrganisation"
+        options={corporateOptions || []}
+      ></Dropdown>
+      {formikProps.values.contactHealthcareOrganisation === 'other' && (
+        <StyledInput
+          containerClass={classNames(Style.address, 'mt-0')}
+          className={classNames(Style.address)}
+          placeholder="University Affiliation"
+          formikProps={formikProps}
+          formikKey="contactOtherHealthcareOrganization"
+          fullWidth
+        ></StyledInput>
+      )}
+    </Fragment>
+  );
+};
