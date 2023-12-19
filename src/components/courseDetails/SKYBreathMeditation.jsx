09a0298976d3bbaa2ea821fc23f2904e5e49345d
Praveen <praveengupta@cloudalgo.com>
2023-12-19 20:53:13 +0530

    fix: update register image to button

--- src/components/courseDetails/SKYBreathMeditation.jsx
+++ src/components/courseDetails/SKYBreathMeditation.jsx
@@ -639,12 +639,9 @@ export const SKYBreathMeditation = ({ data, mode: courseViewMode }) => {
                 </div>
               </div>
               <div className="bar-right">
-                <a href="#" onClick={handleRegister}>
-                  <img
-                    src="/img/regiter-btn-alt.original.png"
-                    alt="Enroll now"
-                  />
-                </a>
+                <button className="register-button" onClick={handleRegister}>
+                  Register Now <FaArrowRightLong className="fa-solid" />
+                </button>
               </div>
             </div>
           </div>
