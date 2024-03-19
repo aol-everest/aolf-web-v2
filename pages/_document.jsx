f2d9224fb4c32a5e89c83b168618dbb01d9fc4b8
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-03-19 22:54:52 +0530

    fix: fixed address search component

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -93,10 +93,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             defer="defer"
           ></script>
 
-          <Script
-            strategy="lazyOnload"
-            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}&libraries=geometry,drawing,places`}
-          ></Script>
+          <script
+            async
+            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY}&libraries=geometry,drawing,places&loading=async&callback=initMap`}
+          ></script>
         </Head>
         <body>
           <Main />
