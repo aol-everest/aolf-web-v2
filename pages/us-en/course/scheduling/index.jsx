cd7e50793c10747b4ff09898624525cf07a4f21d
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-04-16 15:53:08 +0530

    feat: added view item event

--- pages/us-en/course/scheduling/index.jsx
+++ pages/us-en/course/scheduling/index.jsx
@@ -109,6 +109,45 @@ const New = () => {
       }
       if (!showWorkshopSelectModal) {
         setShowWorkshopSelectModal(true);
+        track(
+          'view_item',
+          {
+            ecommerce: {
+              currency: 'USD',
+              value: workshopMaster?.unitPrice,
+              course_format: workshopMaster?.productTypeId,
+              course_name: workshopMaster?.title,
+              items: [
+                {
+                  item_id: 'NA',
+                  item_name: workshopMaster?.title,
+                  affiliation: 'NA',
+                  coupon: '',
+                  discount: 0.0,
+                  index: 0,
+                  item_brand: workshopMaster?.orgnization,
+                  item_category: workshopMaster?.title,
+                  item_category2: workshopMaster?.mode,
+                  item_category3: 'paid',
+                  item_category4: 'NA',
+                  item_category5: 'NA',
+                  item_list_id: workshopMaster?.productTypeId,
+                  item_list_name: workshopMaster?.title,
+                  item_variant: 'NA',
+                  location_id: 'NA',
+                  price: workshopMaster?.unitPrice,
+                  quantity: 1,
+                },
+              ],
+            },
+          },
+          {
+            plugins: {
+              all: false,
+              'gtm-ecommerce-plugin': true,
+            },
+          },
+        );
       }
       getWorkshops();
     }
