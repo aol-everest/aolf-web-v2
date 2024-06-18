15f19b2429e1a2dac586879a2c424eac3746bd3c
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-06-18 20:07:30 +0530

    fix: ui fix

--- pages/us-en/meetup/index.jsx
+++ pages/us-en/meetup/index.jsx
@@ -392,11 +392,13 @@ const Popup = (props) => {
         )}
         <label>{label}</label>
         <button
-          className="courses-filter__button"
+          className={classNames('courses-filter__button', {
+            '!tw-text-slate-300': !buttonText,
+          })}
           data-filter="event-type"
           onClick={!showList ? handleSelectFilter : handleDropdownClick}
         >
-          {buttonText || label}
+          {buttonText || 'Select...'}
         </button>
         {showList && (
           <div className="courses-filter__wrapper-list">
@@ -1267,7 +1269,7 @@ const Meetup = () => {
                 <label>Dates</label>
                 <div className="courses-filter__button date-picker">
                   <DateRangePicker
-                    placeholder="Dates"
+                    placeholder="Select..."
                     appearance="subtle"
                     showHeader={false}
                     onChange={onDatesChange}
