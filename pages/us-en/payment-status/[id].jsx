679e8c8e240cd047b113785de27ce7977d3906ba
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-06-24 12:04:01 +0530

    fix: added payment status page

---
+++ pages/us-en/payment-status/[id].jsx
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { api } from '@utils';
+import { useRouter } from 'next/router';
+
+const checkPaymentStatus = async (id) => {
+  const response = await api.get({
+    path: 'workshopDetail',
+    param: {
+      id,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const data = await response.json();
+  return data;
+};
+
+const retryPaymentStatusCheck = async (
+  id,
+  maxRetries = 5,
+  initialDelay = 1000,
+) => {
+  let attempts = 0;
+  let delay = initialDelay;
+
+  while (attempts < maxRetries) {
+    try {
+      const { status } = await checkPaymentStatus(id);
+      if (status === 'success') {
+        return status; // If payment is successful, return the status
+      }
+      attempts++;
+      console.log(`Attempt ${attempts}: Payment status is ${status}`);
+      if (attempts >= maxRetries) {
+        throw new Error(`Failed after ${maxRetries} attempts`);
+      }
+      // Exponential backoff
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      delay *= 2;
+    } catch (error) {
+      attempts++;
+      console.error(`Attempt ${attempts} failed: ${error.message}`);
+      if (attempts >= maxRetries) {
+        throw new Error(`Failed after ${maxRetries} attempts`);
+      }
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      delay *= 2;
+    }
+  }
+};
+
+const PaymentStatus = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const { id } = router.query;
+  const next = searchParams.get('search');
+
+  const [status, setStatus] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      try {
+        const result = await retryPaymentStatusCheck(id);
+        setStatus(result);
+        router.replace(next);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchStatus();
+  }, [id]);
+
+  return (
+    <main class="login-register-page">
+      <section class="section-login-register">
+        <div class="loading-overlay no-bg">
+          <div class="overlay-loader"></div>
+          <div class="loading-text">
+            <p class="tw-font-bold tw-py-5">Processing payment...</p>
+            <p>
+              Please wait a few minutes. We appreciate your patience during this
+              time!
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+PaymentStatus.noHeader = true;
+PaymentStatus.hideFooter = true;
+
+export default PaymentStatus;
