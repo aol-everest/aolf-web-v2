5a708ed4ef098e604475d328e5eeb9ed241c9f40
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-08-02 16:56:21 +0530

    feat: added daily sky html page

---
+++ pages/us-en/daily-sky/index.jsx
@@ -0,0 +1,174 @@
+import React from 'react';
+
+const DailySKY = () => {
+  return (
+    <main class="daily-sky">
+      <section class="title-header">
+        <h1 class="page-title">Daily SKY</h1>
+      </section>
+      <section class="section-daily-sky-courses">
+        <div class="container">
+          <div class="ds-course-listing">
+            <div class="ds-course-item">
+              <div class="ds-image-wrap">
+                <img src="/img/ds-course-preview-1.webp" alt="course" />
+              </div>
+              <div class="ds-course-header">
+                <div class="play-time">00:48:15</div>
+                <div class="lock-info">
+                  <span class="icon-aol iconaol-lock"></span>
+                </div>
+              </div>
+              <div class="ds-course-info">
+                <div class="ds-course-title">
+                  Daily SKY Practice with Annelies
+                </div>
+                <div class="ds-course-author">Annelies Richmond</div>
+              </div>
+            </div>
+            <div class="ds-course-item">
+              <div class="ds-image-wrap">
+                <img src="/img/ds-course-preview-2.webp" alt="course" />
+              </div>
+              <div class="ds-course-header">
+                <div class="play-time">05:53</div>
+                <div class="lock-info">
+                  <span class="icon-aol iconaol-lock"></span>
+                </div>
+              </div>
+              <div class="ds-course-info">
+                <div class="ds-course-title">
+                  Daily SKY Practice with Jim Larsen
+                </div>
+                <div class="ds-course-author">Jim Larsen</div>
+              </div>
+            </div>
+            <div class="ds-course-item">
+              <div class="ds-image-wrap">
+                <img src="/img/ds-course-preview-3.webp" alt="course" />
+              </div>
+              <div class="ds-course-header">
+                <div class="play-time">05:53</div>
+                <div class="lock-info">
+                  <span class="icon-aol iconaol-lock"></span>
+                </div>
+              </div>
+              <div class="ds-course-info">
+                <div class="ds-course-title">
+                  Daily SKY Practice with Diego (Spanish)
+                </div>
+                <div class="ds-course-author">Diego</div>
+              </div>
+            </div>
+            <div class="ds-course-item">
+              <div class="ds-image-wrap">
+                <img src="/img/ds-course-preview-4.webp" alt="course" />
+              </div>
+              <div class="ds-course-header">
+                <div class="play-time">05:53</div>
+                <div class="lock-info">
+                  <span class="icon-aol iconaol-lock"></span>
+                </div>
+              </div>
+              <div class="ds-course-info">
+                <div class="ds-course-title">
+                  Daily SKY Practice with Shobha (Spanish)
+                </div>
+                <div class="ds-course-author">Shobha</div>
+              </div>
+            </div>
+            <div class="ds-course-item">
+              <div class="ds-image-wrap">
+                <img src="/img/ds-course-preview-5.webp" alt="course" />
+              </div>
+              <div class="ds-course-header">
+                <div class="play-time">05:53</div>
+                <div class="lock-info">
+                  <span class="icon-aol iconaol-lock"></span>
+                </div>
+              </div>
+              <div class="ds-course-info">
+                <div class="ds-course-title">3 Stage Pranayama</div>
+                <div class="ds-course-author">Jim Larsen</div>
+              </div>
+            </div>
+            <div class="ds-course-item">
+              <div class="ds-image-wrap">
+                <img src="/img/ds-course-preview-6.webp" alt="course" />
+              </div>
+              <div class="ds-course-header">
+                <div class="play-time">05:53</div>
+                <div class="lock-info">
+                  <span class="icon-aol iconaol-lock"></span>
+                </div>
+              </div>
+              <div class="ds-course-info">
+                <div class="ds-course-title">Bhastrika</div>
+                <div class="ds-course-author">Jim Larsen</div>
+              </div>
+            </div>
+            <div class="ds-course-item">
+              <div class="ds-image-wrap">
+                <img src="/img/ds-course-preview-7.webp" alt="course" />
+              </div>
+              <div class="ds-course-header">
+                <div class="play-time">05:53</div>
+                <div class="lock-info">
+                  <span class="icon-aol iconaol-lock"></span>
+                </div>
+              </div>
+              <div class="ds-course-info">
+                <div class="ds-course-title">Om & SKY Breath</div>
+                <div class="ds-course-author">Jim Larsen</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section-journey-app">
+        <div class="container">
+          <div class="mobile-app-placeholder">
+            <img
+              src="/img/journey-app-mobile-placeholder.webp"
+              alt="journey mobile app"
+            />
+          </div>
+          <div class="mobile-app-info">
+            <div class="info-header">
+              <div class="app-icon">
+                <img src="/img/logo-icon-small.webp" alt="logo" />
+              </div>
+              <div class="app-title">
+                <h2>Art of living</h2>
+                <h3>Journey App</h3>
+              </div>
+            </div>
+            <ul class="info-list-item">
+              <li>Guided daily SKY Breath Meditation</li>
+              <li>Deepen your experience with SKY Journey</li>
+              <li>Growing online library of meditations & insights</li>
+              <li>Get better rest with sleep support meditations</li>
+              <li>Access live meetups with your instructors</li>
+            </ul>
+            <div class="app-donwload-details">
+              <h4>Download the Journey App</h4>
+              <div class="store-list">
+                <a href="#">
+                  <img src="/img/mobile-app-store-ios.webp" alt="app store" />
+                </a>
+                <a href="#">
+                  <img
+                    src="/img/mobile-app-store-android.webp"
+                    alt="google play"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default DailySKY;
