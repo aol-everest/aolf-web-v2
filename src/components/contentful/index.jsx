f91ab2f81d38d4468cd730fcc15042d3a95dde44
Praveen <praveengupta@cloudalgo.com>
2024-08-09 22:18:42 +0530

    fix: minor change to contentful

--- src/components/contentful/index.jsx
+++ src/components/contentful/index.jsx
@@ -2,7 +2,7 @@ import { createClient } from 'contentful';
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN || '',
 });
 
 export const fetchContentfulDataDetails = async (entryId) => {
