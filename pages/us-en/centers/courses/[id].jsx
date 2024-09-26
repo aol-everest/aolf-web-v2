ababe3fef570ca6ff2146b7e49f93f5fd50fa348
rajesh <sugguna.rajesh70@gmail.com>
2024-09-26 19:32:39 +0530

    fix: resolved comments

--- pages/us-en/centers/courses/[id].jsx
+++ pages/us-en/centers/courses/[id].jsx
@@ -638,12 +638,6 @@ const Course = ({ centerDetail }) => {
             ))}
           </>
         )}
-        <div ref={ref} style={{ flex: '0 0 100%' }}></div>
-        {isSuccess && !hasNextPage && data.pages[0].data.length > 0 && (
-          <div className="no-course-found-wrap">
-            <p></p>
-          </div>
-        )}
       </>
     );
   };
