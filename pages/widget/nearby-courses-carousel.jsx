2655ad5032cacf0134108db15469bd6bcfe25a68
Sandeep Kumar <sandeepkhoj@gmail.com>
2024-10-18 16:34:32 +0530

    fix: - fixed membership thankyou page

---
+++ pages/widget/nearby-courses-carousel.jsx
@@ -0,0 +1,315 @@
+import React from 'react';
+
+const NearbyCoursesCarousel = () => {
+  return (
+    <section class="courses-nearby">
+      <div class="container">
+        <h2 class="courses-nearby__title section-title text-center">
+          Courses happening near you
+        </h2>
+
+        <div class="course-location-select">
+          <div class="form-item">
+            <input type="text" placeholder="Enter a ZIP code" />
+          </div>
+        </div>
+        <div class="courses-slider">
+          <div class="slide">
+            <div class="course-item-box">
+              <div class="course-item-top">
+                <div class="row">
+                  <div class="course-item-date">17 Sep - 19 Sep</div>
+                  <div class="course-item-price">$100</div>
+                </div>
+                <div class="payment-details">
+                  <div class="payby">
+                    Pay As Low As{' '}
+                    <img src="/img/logo-affirm.webp" height="22" />
+                  </div>
+                  <div class="price-breakup">
+                    <div class="price-per-month">
+                      $27/<span>mon</span>
+                    </div>
+                    <div class="payment-tenure">for 12 months</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-item-content">
+                <div class="course-date">
+                  <div>
+                    <label>Timing:</label>
+                    <div>Tu: 12:00 PM-2:30 PM ET</div>
+                    <div>We: 12:00 PM-2:30 PM ET</div>
+                    <div>Th: 12:00 PM-2:30 PM ET</div>
+                  </div>
+                </div>
+                <div class="course-location">
+                  <div>
+                    <label>Location:</label>
+                    <div>13473 Dolomite DR Frisko, TX 75035</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-actions">
+                <button class="btn-primary">Register Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="course-item-box">
+              <div class="course-item-top">
+                <div class="row">
+                  <div class="course-item-date">17 Sep - 19 Sep</div>
+                  <div class="course-item-price">$100</div>
+                </div>
+                <div class="payment-details">
+                  <div class="payby">
+                    Pay As Low As{' '}
+                    <img src="/img/logo-affirm.webp" height="22" />
+                  </div>
+                  <div class="price-breakup">
+                    <div class="price-per-month">
+                      $27/<span>mon</span>
+                    </div>
+                    <div class="payment-tenure">for 12 months</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-item-content">
+                <div class="course-date">
+                  <div>
+                    <label>Timing:</label>
+                    <div>Tu: 12:00 PM-2:30 PM ET</div>
+                    <div>We: 12:00 PM-2:30 PM ET</div>
+                    <div>Th: 12:00 PM-2:30 PM ET</div>
+                  </div>
+                </div>
+                <div class="course-location">
+                  <div>
+                    <label>Location:</label>
+                    <div>13473 Dolomite DR Frisko, TX 75035</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-actions">
+                <button class="btn-primary">Register Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="course-item-box">
+              <div class="course-item-top">
+                <div class="row">
+                  <div class="course-item-date">17 Sep - 19 Sep</div>
+                  <div class="course-item-price">$100</div>
+                </div>
+                <div class="payment-details">
+                  <div class="payby">
+                    Pay As Low As{' '}
+                    <img src="/img/logo-affirm.webp" height="22" />
+                  </div>
+                  <div class="price-breakup">
+                    <div class="price-per-month">
+                      $27/<span>mon</span>
+                    </div>
+                    <div class="payment-tenure">for 12 months</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-item-content">
+                <div class="course-date">
+                  <div>
+                    <label>Timing:</label>
+                    <div>Tu: 12:00 PM-2:30 PM ET</div>
+                    <div>We: 12:00 PM-2:30 PM ET</div>
+                    <div>Th: 12:00 PM-2:30 PM ET</div>
+                  </div>
+                </div>
+                <div class="course-location">
+                  <div>
+                    <label>Location:</label>
+                    <div>13473 Dolomite DR Frisko, TX 75035</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-actions">
+                <button class="btn-primary">Register Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="course-item-box">
+              <div class="course-item-top">
+                <div class="row">
+                  <div class="course-item-date">17 Sep - 19 Sep</div>
+                  <div class="course-item-price">$100</div>
+                </div>
+                <div class="payment-details">
+                  <div class="payby">
+                    Pay As Low As{' '}
+                    <img src="/img/logo-affirm.webp" height="22" />
+                  </div>
+                  <div class="price-breakup">
+                    <div class="price-per-month">
+                      $27/<span>mon</span>
+                    </div>
+                    <div class="payment-tenure">for 12 months</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-item-content">
+                <div class="course-date">
+                  <div>
+                    <label>Timing:</label>
+                    <div>Tu: 12:00 PM-2:30 PM ET</div>
+                    <div>We: 12:00 PM-2:30 PM ET</div>
+                    <div>Th: 12:00 PM-2:30 PM ET</div>
+                  </div>
+                </div>
+                <div class="course-location">
+                  <div>
+                    <label>Location:</label>
+                    <div>13473 Dolomite DR Frisko, TX 75035</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-actions">
+                <button class="btn-primary">Register Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="course-item-box">
+              <div class="course-item-top">
+                <div class="row">
+                  <div class="course-item-date">17 Sep - 19 Sep</div>
+                  <div class="course-item-price">$100</div>
+                </div>
+                <div class="payment-details">
+                  <div class="payby">
+                    Pay As Low As{' '}
+                    <img src="/img/logo-affirm.webp" height="22" />
+                  </div>
+                  <div class="price-breakup">
+                    <div class="price-per-month">
+                      $27/<span>mon</span>
+                    </div>
+                    <div class="payment-tenure">for 12 months</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-item-content">
+                <div class="course-date">
+                  <div>
+                    <label>Timing:</label>
+                    <div>Tu: 12:00 PM-2:30 PM ET</div>
+                    <div>We: 12:00 PM-2:30 PM ET</div>
+                    <div>Th: 12:00 PM-2:30 PM ET</div>
+                  </div>
+                </div>
+                <div class="course-location">
+                  <div>
+                    <label>Location:</label>
+                    <div>13473 Dolomite DR Frisko, TX 75035</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-actions">
+                <button class="btn-primary">Register Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="course-item-box">
+              <div class="course-item-top">
+                <div class="row">
+                  <div class="course-item-date">17 Sep - 19 Sep</div>
+                  <div class="course-item-price">$100</div>
+                </div>
+                <div class="payment-details">
+                  <div class="payby">
+                    Pay As Low As{' '}
+                    <img src="./img/logo-affirm.webp" height="22" />
+                  </div>
+                  <div class="price-breakup">
+                    <div class="price-per-month">
+                      $27/<span>mon</span>
+                    </div>
+                    <div class="payment-tenure">for 12 months</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-item-content">
+                <div class="course-date">
+                  <div>
+                    <label>Timing:</label>
+                    <div>Tu: 12:00 PM-2:30 PM ET</div>
+                    <div>We: 12:00 PM-2:30 PM ET</div>
+                    <div>Th: 12:00 PM-2:30 PM ET</div>
+                  </div>
+                </div>
+                <div class="course-location">
+                  <div>
+                    <label>Location:</label>
+                    <div>13473 Dolomite DR Frisko, TX 75035</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-actions">
+                <button class="btn-primary">Register Now</button>
+              </div>
+            </div>
+          </div>
+          <div class="slide">
+            <div class="course-item-box">
+              <div class="course-item-top">
+                <div class="row">
+                  <div class="course-item-date">17 Sep - 19 Sep</div>
+                  <div class="course-item-price">$100</div>
+                </div>
+                <div class="payment-details">
+                  <div class="payby">
+                    Pay As Low As{' '}
+                    <img src="./img/logo-affirm.webp" height="22" />
+                  </div>
+                  <div class="price-breakup">
+                    <div class="price-per-month">
+                      $27/<span>mon</span>
+                    </div>
+                    <div class="payment-tenure">for 12 months</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-item-content">
+                <div class="course-date">
+                  <div>
+                    <label>Timing:</label>
+                    <div>Tu: 12:00 PM-2:30 PM ET</div>
+                    <div>We: 12:00 PM-2:30 PM ET</div>
+                    <div>Th: 12:00 PM-2:30 PM ET</div>
+                  </div>
+                </div>
+                <div class="course-location">
+                  <div>
+                    <label>Location:</label>
+                    <div>13473 Dolomite DR Frisko, TX 75035</div>
+                  </div>
+                </div>
+              </div>
+              <div class="course-actions">
+                <button class="btn-primary">Register Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="courses-nearby-actions">
+          <button class="btn-primary">More Dates</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+NearbyCoursesCarousel.noHeader = true;
+NearbyCoursesCarousel.hideFooter = true;
+
+export default NearbyCoursesCarousel;
