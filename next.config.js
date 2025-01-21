59a4def0840c90fcebf6c6513671f9c376517ad6
Sandeep Kumar <sandeepkhoj@gmail.com>
2025-01-21 23:16:21 +0530

    fix: next config clean

--- next.config.js
+++ next.config.js
@@ -51,14 +51,6 @@ const moduleExports = {
   images: {
     domains: ['images.ctfassets.net'],
   },
-  distDir: '_next',
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
 
   async redirects() {
     return [
