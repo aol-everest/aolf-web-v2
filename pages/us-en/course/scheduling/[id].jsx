3f0abc877611969040a0d80311e74d642219e1da
Sandeep Kumar <sandeepkhoj@gmail.com>
2025-02-18 15:24:48 +0530

    feat: jS-3948

--- pages/us-en/course/scheduling/[id].jsx
+++ pages/us-en/course/scheduling/[id].jsx
@@ -120,6 +120,9 @@ const SchedulingPaymentForm = ({
   const { track, identify } = useAnalytics();
   const { profile = {}, passwordLess, isAuthenticated } = useAuth();
   const { signOut } = passwordLess;
+  const [defaultUserEmail] = useQueryState('email');
+  console.log(defaultUserEmail);
+
   const formRef = useRef();
 
   const [loading, setLoading] = useState(false);
@@ -547,33 +550,38 @@ const SchedulingPaymentForm = ({
     setDiscountResponse(discount);
   };
 
-  const initialValue = {
+  let initialValue = {
     firstName: '',
     lastName: '',
-    email: '',
+    email: defaultUserEmail || '',
     contactAddress: '',
     contactCity: '',
     contactState: '',
     contactZip: '',
     contactPhone: '',
+    questionnaire: questionnaireArray,
+    ppaAgreement: true,
   };
+
+  if (isAuthenticated) {
+    initialValue = {
+      ...initialValue,
+      firstName: first_name,
+      lastName: last_name,
+      email: email,
+      contactAddress: personMailingStreet || '',
+      contactCity: personMailingCity || '',
+      contactState: personMailingState || '',
+      contactZip: personMailingPostalCode || '',
+      contactPhone: personMobilePhone,
+    };
+  }
 
   return (
     <>
       {loading && <Loader />}
       <Formik
-        initialValues={{
-          firstName: first_name,
-          lastName: last_name,
-          email: email,
-          contactAddress: personMailingStreet || '',
-          contactCity: personMailingCity || '',
-          contactState: personMailingState || '',
-          contactZip: personMailingPostalCode || '',
-          contactPhone: personMobilePhone,
-          questionnaire: questionnaireArray,
-          ppaAgreement: true,
-        }}
+        initialValues={initialValue}
         validationSchema={
           activeStep === CheckoutStates.EMAIL_INPUT
             ? emailStepSchema
