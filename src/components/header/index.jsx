39d18c10642b3a8e596a01df966a610a4576b962
neeraj-cloudalgo <neeraj.sharma@cloudalgo.com>
2025-04-15 21:34:18 +0530

    feat: events menu hidden

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -236,16 +236,16 @@ const AOL_MENU = [
       },
     ],
   },
-  {
-    name: 'Events',
-    // submenu: [
-    //   {
-    //     name: 'Mobile, AL',
-    //     link: 'https://event.us.artofliving.org/us-en/lp1/journey-within-alabama/',
-    //     props: { target: '_blank' },
-    //   },
-    // ],
-  },
+  // {
+  //   name: 'Events',
+  //   submenu: [
+  //     {
+  //       name: 'Mobile, AL',
+  //       link: 'https://event.us.artofliving.org/us-en/lp1/journey-within-alabama/',
+  //       props: { target: '_blank' },
+  //     },
+  //   ],
+  // },
   {
     name: 'Centers',
     submenu: [
